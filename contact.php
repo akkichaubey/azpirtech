458ce452fd57b93caf8da44e028cab3bbfb998a9
Akash Chaubey <akashakki9520@gmail.com>
2023-10-07 16:46:06 +0530

    contact page

---
+++ contact.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include("include/head.php"); ?>
+</head>
+
+<body>
+    <!-- header -->
+    <?php include("include/header.php"); ?>
+    <!-- header -->
+    <!-- inner banner -->
+    <div class="inner-banner" style="background-image: url(./assets/images/inner-bg.png);">
+      <div class="container">
+        <div class="banner-info">
+          <h1 class="banner-title h2" data-aos="fade-up">Contact US</h1>
+          <ul class="bread" data-aos="fade-up" data-aos-delay="100">
+            <li><a href="#">Home</a></li>
+            <li>Contact US</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <!-- inner banner -->
+
+    <!-- about -->
+    <div class="section contact">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6">
+            <div class="title">
+              <h2 class="h2" data-aos="fade-up">We would love to hear you</h2>
+            </div>
+            <div class="contact-info" data-aos="fade-up">
+              <ul>
+                <li>
+                  <a href="#">
+                    <div class="icon-box">
+                      <img src="./assets/images/icons/location-icon.png" alt="img">
+                    </div>
+                    <span>Ahemdabad,<br> Gujarat<br> CA 95630</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <div class="icon-box">
+                      <img src="./assets/images/icons/call-icon.png" alt="img">
+                    </div>
+                    <span>916-934-9574</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <div class="icon-box">
+                      <img src="./assets/images/icons/email-icon.png" alt="img">
+                    </div>
+                    <span>info@azpiretech.com</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <div class="contact-form">
+              <form>
+                <div class="input-group">
+                  <span><img src="./assets/images/icons/user-icon.png" alt="icon"></span>
+                  <input class="form-control" placeholder="Name">
+                </div>
+                <div class="input-group">
+                  <span><img src="./assets/images/icons/mail-icon.png" alt="icon"></span>
+                  <input class="form-control" placeholder="Email">
+                </div>
+                <div class="input-group">
+                  <span><img src="./assets/images/icons/phone-icon.png" alt="icon"></span>
+                  <input class="form-control" placeholder="Phone">
+                </div>
+                <div class="input-group">
+                  <span><img src="./assets/images/icons/phone-icon.png" alt="icon"></span>
+                  <textarea class="form-control" placeholder="Message"></textarea>
+                </div>
+                <div class="input-group btn-wrap">
+                  <button type="submit" class="btn btn-sm btn-primary">Submit</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>       
+    <!-- about -->
+
+    <!-- map -->
+    <div class="section map">
+      <div class="map-box">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d235013.74843649793!2d72.4149335523415!3d23.020474097240065!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e848aba5bd449%3A0x4fcedd11614f6516!2sAhmedabad%2C%20Gujarat!5e0!3m2!1sen!2sin!4v1696666986338!5m2!1sen!2sin" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+    </div>
+    <!-- map -->
+
+    
+
+   
+
+   
+
+    <!--footer-->
+    <?php include("include/footer.php"); ?>
+    <!--footer-->
+</body>
+</html>
